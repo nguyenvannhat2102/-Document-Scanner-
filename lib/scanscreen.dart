6e7f3bd94d1cb5bfb47a9c6158d77641e32ca5ea
nguyenvannhat2102 <nguyenvannhat21022002@gmail.com>
2024-06-01 15:31:10 +0700

    complete scan

--- lib/scanscreen.dart
+++ lib/scanscreen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_mlkit_entity_extraction/google_mlkit_entity_extraction.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 
@@ -19,6 +20,7 @@ class _ScanScreenState extends State<ScanScreen> {
   late EntityExtractor entityExtractor;
   String result = "";
   List<EntityDM> entitiesList = [];
+
   @override
   void initState() {
     super.initState();
@@ -26,6 +28,12 @@ class _ScanScreenState extends State<ScanScreen> {
     entityExtractor =
         EntityExtractor(language: EntityExtractorLanguage.english);
     doTextRecognition();
+  }
+
+  @override
+  void dispose() {
+    // Hủy bỏ các tài nguyên
+    super.dispose();
   }
 
   doTextRecognition() async {
@@ -74,41 +82,95 @@ class _ScanScreenState extends State<ScanScreen> {
                   const EdgeInsets.only(top: 10, bottom: 10, left: 5, right: 5),
               child: Image.file(this.widget.image),
             ),
-            Card(
-              margin: const EdgeInsets.all(10),
-              color: Colors.grey.shade300,
-              child: Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
+            // Image.file(this.widget.image),
+            ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  child: Container(
+                    height: 70,
+                    color: Colors.grey.shade300,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        const Icon(Icons.document_scanner),
-                        const Text(
-                          'Result',
-                          style: TextStyle(fontSize: 18),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15),
+                          child: Icon(
+                            entitiesList[index].iconData,
+                            size: 25,
+                            color: Colors.black,
+                          ),
                         ),
-                        InkWell(
-                          onTap: () {
-                            Clipboard.setData(ClipboardData(text: result));
-                            SnackBar snackBar =
-                                const SnackBar(content: Text('copied'));
-                            ScaffoldMessenger.of(context)
-                                .showSnackBar(snackBar);
-                          },
-                          child: const Icon(Icons.copy),
+                        Expanded(
+                          child: Text(
+                            entitiesList[index].value,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 15,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 15),
+                          child: InkWell(
+                            onTap: () {
+                              Clipboard.setData(ClipboardData(
+                                  text: entitiesList[index].value));
+                              SnackBar sn =
+                                  const SnackBar(content: Text('copied'));
+                              ScaffoldMessenger.of(context).showSnackBar(sn);
+                            },
+                            child: const Icon(
+                              Icons.copy,
+                              color: Colors.black,
+                            ),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  Text(
-                    result,
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
+                );
+              },
+              itemCount: entitiesList.length,
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
             ),
+            // Card(
+            //   margin: const EdgeInsets.all(10),
+            //   color: Colors.grey.shade300,
+            //   child: Column(
+            //     children: [
+            //       Container(
+            //         padding: const EdgeInsets.all(8),
+            //         child: Row(
+            //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //           children: [
+            //             const Icon(Icons.document_scanner),
+            //             const Text(
+            //               'Result',
+            //               style: TextStyle(fontSize: 18),
+            //             ),
+            //             InkWell(
+            //               onTap: () {
+            //                 Clipboard.setData(ClipboardData(text: result));
+            //                 SnackBar snackBar =
+            //                     const SnackBar(content: Text('copied'));
+            //                 ScaffoldMessenger.of(context)
+            //                     .showSnackBar(snackBar);
+            //               },
+            //               child: const Icon(Icons.copy),
+            //             ),
+            //           ],
+            //         ),
+            //       ),
+            //       Text(
+            //         result,
+            //         style: const TextStyle(fontSize: 18),
+            //       ),
+            //     ],
+            //   ),
+            // ),
           ],
         ),
       ),
@@ -119,6 +181,19 @@ class _ScanScreenState extends State<ScanScreen> {
 class EntityDM {
   String name;
   String value;
+  IconData? iconData;
 
-  EntityDM(this.name, this.value);
+  EntityDM(this.name, this.value) {
+    if (name == 'phone') {
+      iconData = Icons.phone;
+    } else if (name == 'address') {
+      iconData = Icons.location_on;
+    } else if (name == 'email') {
+      iconData = Icons.email;
+    } else if (name == 'url') {
+      iconData = Icons.web;
+    } else {
+      iconData = Icons.ac_unit_outlined;
+    }
+  }
 }
